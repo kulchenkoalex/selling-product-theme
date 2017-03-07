84f72ec135ea1e087a8b4c9fcc0765e8e1af23af
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-07 14:11:56 +0200

    Вывод подвала footer.php

--- footer.php
+++ footer.php
@@ -6,25 +6,45 @@
  * Time: 11:36
  */
 ?>
-</div><!-- #content -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
+<div id="prefooter" role="complementary">
+    <div class="container">
+        <div class="row">
+            <div class="one-third-block">
 
-    <div class="site-footer-widgets">
-        <div class="site-container">
-            <ul>
-                <?php dynamic_sidebar( 'topshop-site-footer' ); ?>
-            </ul>
-            <div class="clearboth"></div>
+                <div class="contact-widget widget" itemscope="" itemtype="http://schema.org/LocalBusiness">
 
-        </div>
-    </div>
+                    <div class="name" itemprop="name">
+                        <strong>Contact information</strong>
+                    </div>
+                        <div class="street" itemprop="streetAddress">Street, European</div>
+                        <div class="city" itemprop="addressLocality">City, Poltava</div>
+                        <div class="country" itemprop="addressCountry">Country, Ukraine</div>
+                    </div> <!-- .address -->
 
-    <div class="clearboth"></div>
-    </div>
+                    <div class="telephone"><i class="fa fa-phone fa-fw"></i> (066) 9937792</div>
+                    <div class="email-address"><i class="fa fa-envelope fa-fw"></i> <a href="contact.html" title="Email us" itemprop="email">kulchenko.alexey@gmail.com</a></div>
+                    <div class="website"><i class="fa fa-globe fa-fw"></i> <a href="http://www.wp8poltava.it-dev-lab.com" title="Visit our website" itemprop="url">wp8poltava.it-dev-lab.com</a></div>
 
-</footer> <!-- .site-footer -->
-</div>  <!-- #page -->
+                </div> <!-- .contact-widget -->
+
+            </div> <!-- .one-third-block -->
+
+        </div> <!-- .row -->
+    </div> <!-- .container -->
+</div> <!-- #prefooter -->
+
+<footer id="footer" role="contentinfo">
+    <div class="container">
+        <div class="row">
+
+            <div class="copyright">&copy; Poltava, Ukraine 2017</div>
+<!--            <div class="attribution">Web Design by <a href="http://www.donnyburnside.com" title="Web Design by Donny Burnside" target="_blank">Donny Burnside</a>.</div>-->
+
+        </div> <!-- .row -->
+    </div> <!-- .container -->
+</footer> <!-- #footer -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
