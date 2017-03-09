a1f769d10acd2ed3aaa583b4e5aebc6400256ef7
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-09 13:36:10 +0200

    Произвольное меню

--- header.php
+++ header.php
@@ -39,3 +39,26 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
+
+<?php
+$args = array(
+    'theme_location' => 'primary',        // (string) Расположение меню в шаблоне. (указывается ключ которым было зарегистрировано меню в функции register_nav_menus)
+    'menu'            => '',              // (string) Название выводимого меню (указывается в админке при создании меню, приоритетнее
+    // чем указанное местоположение theme_location - если указано, то параметр theme_location игнорируется)
+    'container'       => 'nav',           // (string) Контейнер меню. Обворачиватель ul. Указывается тег контейнера (по умолчанию в тег div)
+    'container_class' => '',              // (string) class контейнера (div тега)
+    'container_id'    => 'main-menu',              // (string) id контейнера (div тега)
+    'menu_class'      => 'horizontal-navigation',          // (string) class самого меню (ul тега)
+    'menu_id'         => '',              // (string) id самого меню (ul тега)
+    'echo'            => true,            // (boolean) Выводить на экран или возвращать для обработки
+    'fallback_cb'     => 'wp_page_menu',  // (string) Используемая (резервная) функция, если меню не существует (не удалось получить)
+    //'before'          => '',              // (string) Текст перед <a> каждой ссылки
+    //'after'           => '',              // (string) Текст после </a> каждой ссылки
+    //'link_before'     => '',              // (string) Текст перед анкором (текстом) ссылки
+    //'link_after'      => '',              // (string) Текст после анкора (текста) ссылки
+    //'depth'           => 0,               // (integer) Глубина вложенности (0 - неограничена, 2 - двухуровневое меню)
+    //'walker'          => '',              // (object) Класс собирающий меню. Default: new Walker_Nav_Menu
+
+);
+wp_nav_menu($args);
+?>
