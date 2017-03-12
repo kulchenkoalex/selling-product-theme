f766d061c497c8eb4702b1c2659535e6229320ed
kulchenkoalex <alexej07@bk.ru>
2017-03-12 21:23:47 +0200

    Отображение sidebar на страницах сайта

--- sidebar.php
+++ sidebar.php
@@ -5,10 +5,7 @@
  * Date: 02.03.2017
  * Time: 13:05
  */
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-    return;
-} ?>
-
-<div id="secondary" class="widget-area" role="complementary">
-    <?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+    if ( function_exists('dynamic_sidebar') ) {
+    dynamic_sidebar('right-sidebar');
+    }
+?>
