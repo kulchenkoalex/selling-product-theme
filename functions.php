91e76942c9be6c9ffddd9fa70a1cc1ef1a23c328
kulchenkoalex <alexej07@bk.ru>
2017-03-12 21:28:01 +0200

    Отключение стандартных виджетов WordPress

--- functions.php
+++ functions.php
@@ -132,3 +132,20 @@ function register_my_widgets(){
     ) );
 }
 add_action( 'widgets_init', 'register_my_widgets' );
+
+function true_remove_default_widget() {
+    unregister_widget('WP_Widget_Archives'); // Архивы
+    //unregister_widget('WP_Widget_Calendar'); // Календарь
+    //unregister_widget('WP_Widget_Categories'); // Рубрики
+    unregister_widget('WP_Widget_Meta'); // Мета
+    //unregister_widget('WP_Widget_Pages'); // Страницы
+    //unregister_widget('WP_Widget_Recent_Comments'); // Свежие комментарии
+    //unregister_widget('WP_Widget_Recent_Posts'); // Свежие записи
+    //unregister_widget('WP_Widget_RSS'); // RSS
+    //unregister_widget('WP_Widget_Search'); // Поиск
+    unregister_widget('WP_Widget_Tag_Cloud'); // Облако меток
+    //unregister_widget('WP_Widget_Text'); // Текст
+    //unregister_widget('WP_Nav_Menu_Widget'); // Произвольное меню
+}
+ 
+add_action( 'widgets_init', 'true_remove_default_widget', 20 );
